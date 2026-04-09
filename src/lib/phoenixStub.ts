54c7514e30ff34728695c6e688a477d95e36d4c1
kavecontactomusic-web <kave.contactomusic@gmail.com>
2026-04-09 17:00:10 -0500

    fix: proper phoenix stub for realtime

--- src/lib/phoenixStub.ts
+++ src/lib/phoenixStub.ts
@@ -1,1 +1,26 @@
-export default {};
+export class Socket {
+  constructor() {}
+  connect() {}
+  disconnect() {}
+  channel() { return new Channel(); }
+  onOpen() {}
+  onClose() {}
+  onError() {}
+}
+
+export class Channel {
+  constructor() {}
+  join() { return { receive: () => this }; }
+  leave() { return { receive: () => this }; }
+  on() {}
+  off() {}
+  push() { return { receive: () => this }; }
+}
+
+export class Presence {
+  constructor() {}
+  onSync() {}
+  list() { return []; }
+}
+
+export default { Socket, Channel, Presence };
